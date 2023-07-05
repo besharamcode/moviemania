1c56dcef0e3c282767f381c35c283aad8d0277f9
Besharam <Kushwahmohit3512@gmail.com>
2023-07-05 19:58:52 +0530

    second commit

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -5,14 +5,12 @@ const Search = () => {
   const { query, setQuery } = useContext(movieContext);
   return (
     <div className="search-bar">
-      <form onSubmit={(e) => e.preventDefault} action="#">
       <input
         type="text"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         placeholder="Enter movie name..."
       />
-      </form>
     </div>
   );
 };
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,5 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
 import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from 'vite'
-
-// https://vitejs.dev/config/
-// export default defineConfig({
-
-// });
-
 
 export default defineConfig({
   plugins: [react()]
